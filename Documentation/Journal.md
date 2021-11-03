1a990babef23358e6c0af8e44cd88ab55669c123
Sebastian Faubel <sebastian@semiodesk.com>
2021-11-03 12:12:09 +0100

    Finished database installation.

--- Documentation/Journal.md
+++ Documentation/Journal.md
@@ -10,8 +10,25 @@
   - Creating docker-compose.yml
   - Add [ASP.NET](https://hub.docker.com/_/microsoft-dotnet-framework-aspnet) 4.8 virtual machine
   - Add [Microsoft SQL Server](https://hub.docker.com/_/microsoft-mssql-server) 2019 virtual machine
-- Install [Azure Data Studio](https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver15) and setup ProfilesRMS database
+- Install [Microsoft SQL Server Management Studio](https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver15) (SSMS) and setup ProfilesRMS database
   - Execute ProfilesRNS_CreateDatabase.sql
   - Execute ProfilesRNS_CreateSchema.sql
   - Execute ProfilesRNS_CreateAccount.sql (modified password)
   - Copy files from Database\Data\* into db server directory /var/opt/mssql/data/ProfilesRNS/
+- Project kick-off meeting with Antonella Succurro
+
+### Tuesday, 3rd November [Sebastian Faubel](mailto:sebastian@semiodesk.com)
+
+- Switching from Docker-based SQL Server 2019 to locally installed Developer Edition
+  - Reason: The system requires the use of _Integration Services_ which are not available in the Docker images
+  - Enable Integration Services in setup
+  - Enable Full Text Search in setup
+- Ensure to run SSMS as Administrator to be able to connect with Integration Services (Install Guide p. 12)
+- Ensure that the SQL Server Agent service is started before proceeding
+- Modifying SQL job scripts
+  - Replacing $(YourProfilesServerName) with "localhost" (including quotes) as the server runs on the development machine.
+  - Replacing $(YourProfilesDatabaseName) with "ProfilesRNS" (including quotes)
+- After success, 4 new jobs are listed in localhost -> SQL Server Agent -> Jobs
+- Created temporary Google Maps API key for geocoding billed to Semiodesk
+- When setting up the nighly job, ensure that the SQL command is correct (not as displayed in the manual):
+  - EXEC [Framework.].[RunJobGroup] @JobGroup = 4
--- src/ProfilesRNS/Database/ProfilesRNS_DataLoad_Part1.sql
+++ src/ProfilesRNS/Database/ProfilesRNS_DataLoad_Part1.sql
@@ -13,12 +13,13 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER (PRESIDENT AND FELLOWS OF HARV
 */
 -- Step 1 Load base data files
 -- To run this script manually, replace the $(ProfilesRNSRootPath)\Data to the location that contains the various XML files (e.g. c:\)
-EXEC [Framework.].[LoadXMLFile] @FilePath = '/var/opt/mssql/data/ProfilesRNS/InstallData.xml', @TableDestination = '[Framework.].[InstallData]', @DestinationColumn = 'DATA'
-EXEC [Framework.].[LoadXMLFile] @FilePath = '/var/opt/mssql/data/ProfilesRNS/VIVO_1.4.owl', @TableDestination = '[Ontology.Import].owl', @DestinationColumn = 'DATA', @NameValue = 'VIVO_1.4'
-EXEC [Framework.].[LoadXMLFile] @FilePath = '/var/opt/mssql/data/ProfilesRNS/PRNS_1.4.owl', @TableDestination = '[Ontology.Import].owl', @DestinationColumn = 'DATA', @NameValue = 'PRNS_1.4'
-EXEC [Framework.].[LoadXMLFile] @FilePath = '/var/opt/mssql/data/ProfilesRNS/SemGroups.xml', @TableDestination = '[Profile.Data].[Concept.Mesh.File]', @DestinationColumn = 'DATA', @NameValue = 'SemGroups.xml'
-EXEC [Framework.].[LoadXMLFile] @FilePath = '/var/opt/mssql/data/ProfilesRNS/SemTypes.xml', @TableDestination = '[Profile.Data].[Concept.Mesh.File]', @DestinationColumn = 'DATA', @NameValue = 'SemTypes.xml'
-EXEC [Framework.].[LoadXMLFile] @FilePath = '/var/opt/mssql/data/ProfilesRNS/MeSH.xml', @TableDestination = '[Profile.Data].[Concept.Mesh.File]', @DestinationColumn = 'DATA', @NameValue = 'MeSH.xml'
+EXEC [Framework.].[LoadXMLFile] @FilePath = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\ProfilesRNS\InstallData.xml', @TableDestination = '[Framework.].[InstallData]', @DestinationColumn = 'DATA'
+EXEC [Framework.].[LoadXMLFile] @FilePath = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\ProfilesRNS\VIVO_1.4.owl', @TableDestination = '[Ontology.Import].owl', @DestinationColumn = 'DATA', @NameValue = 'VIVO_1.4'
+EXEC [Framework.].[LoadXMLFile] @FilePath = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\ProfilesRNS\PRNS_1.4.owl', @TableDestination = '[Ontology.Import].owl', @DestinationColumn = 'DATA', @NameValue = 'PRNS_1.4'
+EXEC [Framework.].[LoadXMLFile] @FilePath = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\ProfilesRNS\SemGroups.xml', @TableDestination = '[Profile.Data].[Concept.Mesh.File]', @DestinationColumn = 'DATA', @NameValue = 'SemGroups.xml'
+EXEC [Framework.].[LoadXMLFile] @FilePath = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\ProfilesRNS\SemTypes.xml', @TableDestination = '[Profile.Data].[Concept.Mesh.File]', @DestinationColumn = 'DATA', @NameValue = 'SemTypes.xml'
+EXEC [Framework.].[LoadXMLFile] @FilePath = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\ProfilesRNS\MeSH.xml', @TableDestination = '[Profile.Data].[Concept.Mesh.File]', @DestinationColumn = 'DATA', @NameValue = 'MeSH.xml'
 EXEC [Framework.].[LoadInstallData]
 UPDATE [Framework.].[Parameter] SET Value = '$(ProfilesRNSBasePath)' WHERE ParameterID = 'basePath'
+UPDATE [Framework.].[Parameter] SET Value = '41bfbbab-0349-4463-b350-1eb26a349bec' WHERE ParameterID = 'RC4EncryptionKey'
 EXEC [Framework.].[RunJobGroup] @JobGroup = 1
