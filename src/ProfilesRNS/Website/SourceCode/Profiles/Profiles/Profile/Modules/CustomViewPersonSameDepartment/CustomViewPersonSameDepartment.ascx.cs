9f088f477b53b54ae0ba1fb86daefe6ce49b8d85
Sebastian Faubel <sebastian@semiodesk.com>
2022-01-17 15:23:16 +0100

    Explore button now working on same department custom view.

--- src/ProfilesRNS/Website/SourceCode/Profiles/Profiles/Profile/Modules/CustomViewPersonSameDepartment/CustomViewPersonSameDepartment.ascx.cs
+++ src/ProfilesRNS/Website/SourceCode/Profiles/Profiles/Profile/Modules/CustomViewPersonSameDepartment/CustomViewPersonSameDepartment.ascx.cs
@@ -70,11 +70,15 @@ namespace Profiles.Profile.Modules.CustomViewPersonSameDepartment
             if (litListItem != null)
                 litListItem.Text = "<li><a href='" + x.URI + "'>" + x.Name + "</a></li>";
 
-            //if (e.Item.ItemType == ListItemType.Footer)
-            //{
-            //    HyperLink moreurl = (HyperLink)e.Item.FindControl("moreurl");
-            //    moreurl.NavigateUrl = Root.Domain + "/search/default.aspx?searchtype=people&searchfor=&SearchRequest=" + this.SearchRequest;
-            //}
+            if (e.Item.ItemType == ListItemType.Header)
+            {
+                HyperLink moreurl = (HyperLink)e.Item.FindControl("moreurl");
+
+                if (moreurl != null)
+                {
+                    moreurl.NavigateUrl = Root.Domain + "/search/default.aspx?searchtype=people&searchfor=&SearchRequest=" + this.SearchRequest;
+                }
+            }
 
         }
 
